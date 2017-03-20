c35fde91581ecb2c81c236fe4fb460e559a901c6
Ievgen Ponomarenko <kikomdev@gmail.com>
2017-03-20 16:14:14 +0200

    Added allowed header "Authorization" to api cors

--- src/api/api.go
+++ src/api/api.go
@@ -43,7 +43,7 @@ func Start(cfg config.ApiConfig) {
 		corsConfig.AllowAllOrigins = true
 		corsConfig.AllowCredentials = true
 		corsConfig.AllowMethods = []string{"PUT", "POST", "DELETE", "GET", "OPTIONS"}
-		corsConfig.ExposeHeaders = []string{"Authorization"}
+		corsConfig.AllowHeaders = []string{"Origin", "Authorization"}
 
 		app.Use(cors.New(corsConfig))
 		log.Info("API CORS enabled")
