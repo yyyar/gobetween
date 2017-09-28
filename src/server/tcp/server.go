5f24e12787585a3ea8018e3ff51fbab694efceb5
Yaroslav Pogrebnyak <yyyaroslav@gmail.com>
2017-09-28 11:29:27 +0300

    Added validation, more logging and extendend config for further

--- src/server/tcp/server.go
+++ src/server/tcp/server.go
@@ -342,14 +342,24 @@ func (this *Server) handle(ctx *core.TcpContext) {
 	this.scheduler.IncrementConnection(*backend)
 	defer this.scheduler.DecrementConnection(*backend)
 
-	/* Stat proxying */
-	if this.cfg.ProxyProtocol == "v1" {
-		log.Debug("Sending proxy-protocol header ", clientConn.RemoteAddr(), " -> ", this.listener.Addr(), " -> ", backendConn.RemoteAddr())
-		err := proxyprotocol.SendProxyProtocolV1(clientConn, backendConn)
-		if err != nil {
-			log.Error(err)
+	/* Send proxy protocol header if configured */
+	if this.cfg.ProxyProtocol != nil {
+		switch this.cfg.ProxyProtocol.Version {
+		case "1":
+			log.Debug("Sending proxy_protocol v1 header ", clientConn.RemoteAddr(), " -> ", this.listener.Addr(), " -> ", backendConn.RemoteAddr())
+			err := proxyprotocol.SendProxyProtocolV1(clientConn, backendConn)
+			if err != nil {
+				log.Error(err)
+				return
+			}
+		default:
+			log.Error("Unsupported proxy_protocol version " + this.cfg.ProxyProtocol.Version + ", aborting connection")
+			return
 		}
 	}
+
+	/* ----- Stat proxying ----- */
+
 	log.Debug("Begin ", clientConn.RemoteAddr(), " -> ", this.listener.Addr(), " -> ", backendConn.RemoteAddr())
 	cs := proxy(clientConn, backendConn, utils.ParseDurationOrDefault(*this.cfg.BackendIdleTimeout, 0))
 	bs := proxy(backendConn, clientConn, utils.ParseDurationOrDefault(*this.cfg.ClientIdleTimeout, 0))
