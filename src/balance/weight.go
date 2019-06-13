6d446f67fd16d56f93c6120508677f79288c4523
Illarion Kovalchuk <illarion.kovalchuk@gmail.com>
2019-06-13 13:06:31 +0300

    Implemented priority + weight balancing #204

--- src/balance/weight.go
+++ src/balance/weight.go
@@ -8,6 +8,7 @@ package balance
 
 import (
 	"errors"
+	"fmt"
 	"math/rand"
 
 	"github.com/yyyar/gobetween/core"
@@ -19,7 +20,8 @@ import (
 type WeightBalancer struct{}
 
 /**
- * Elect backend based on weight strategy
+ * Elect backend based on weight with priority strategy.
+ * See https://tools.ietf.org/html/rfc2782, Priority and Weight sections
  */
 func (b *WeightBalancer) Elect(context core.Context, backends []*core.Backend) (*core.Backend, error) {
 
@@ -27,18 +29,50 @@ func (b *WeightBalancer) Elect(context core.Context, backends []*core.Backend) (
 		return nil, errors.New("Can't elect backend, Backends empty")
 	}
 
-	totalWeight := 0
-	for _, backend := range backends {
-		if backend.Weight <= 0 {
-			return nil, errors.New("Invalid backend weight 0")
-		}
-		totalWeight += backend.Weight
+	if len(backends) == 1 {
+		return backends[0], nil
 	}
 
-	r := rand.Intn(totalWeight)
-	pos := 0
+	// according to RFC we should use backends with lowest priority
+	minPriority := backends[0].Priority
+	// group of backends with priority == minPriority
+	group := make([]*core.Backend, 0, len(backends))
+	// sum of weights in the group
+	groupSumWeight := 0
 
 	for _, backend := range backends {
+
+		if backend.Priority > minPriority {
+			continue
+		}
+
+		if backend.Priority < 0 {
+			return nil, fmt.Errorf("Invalid backend priority, shold not be less than 0: %v", backend.Priority)
+		}
+
+		if backend.Weight <= 0 {
+			return nil, fmt.Errorf("Invalid backend weight, should not be less or equal to 0: %v", backend.Weight)
+		}
+
+		// got new lower priority, reset
+		if backend.Priority < minPriority {
+			minPriority = backend.Priority
+			group = make([]*core.Backend, 0, len(backends))
+			groupSumWeight = 0
+		}
+
+		group = append(group, backend)
+		groupSumWeight += backend.Weight
+	}
+
+	if len(group) == 1 {
+		return group[0], nil
+	}
+
+	r := rand.Intn(groupSumWeight)
+	pos := 0
+
+	for _, backend := range group {
 		pos += backend.Weight
 		if r >= pos {
 			continue
