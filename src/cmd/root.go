7f7f4620a303954b19079f700cc0cecc984d4d46
Illarion Kovalchuk <illarion.kovalchuk@gmail.com>
2019-08-03 21:09:21 +0300

    Let gobetween write it's pidfile and fix Systemd unit file issues #190

--- src/cmd/root.go
+++ src/cmd/root.go
@@ -8,9 +8,10 @@ package cmd
 
 import (
 	"fmt"
-
 	"github.com/spf13/cobra"
 	"github.com/yyyar/gobetween/info"
+	"github.com/yyyar/gobetween/utils/pidfile"
+	"os"
 )
 
 /* Persistent parsed options */
@@ -18,6 +19,9 @@ var format string
 
 /* Parsed options */
 var configPath string
+
+/* Pid file path */
+var pidFilePath string
 
 /* Show version */
 var showVersion bool
@@ -31,6 +35,7 @@ var isConfigEnvVars bool
 func init() {
 	RootCmd.Flags().BoolVarP(&showVersion, "version", "v", false, "Print version information and quit")
 	RootCmd.Flags().StringVarP(&configPath, "config", "c", "", "Path to configuration file")
+	RootCmd.PersistentFlags().StringVarP(&pidFilePath, "pidfile", "p", "", "Write pid to specified file")
 	RootCmd.PersistentFlags().StringVarP(&format, "format", "f", "toml", "Configuration file format: \"toml\" or \"json\"")
 	RootCmd.PersistentFlags().BoolVarP(&isConfigEnvVars, "use-config-env-vars", "e", false, "Enable env variables interpretation in config file")
 }
@@ -41,6 +46,14 @@ func init() {
 var RootCmd = &cobra.Command{
 	Use:   "gobetween",
 	Short: "Modern & minimalistic load balancer for the Cloud era",
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		if pidFilePath != "" {
+			if err := pidfile.WritePidFile(pidFilePath); err != nil {
+				fmt.Printf("Unable to write pidfile %s: %v\n", pidFilePath, err)
+				os.Exit(1)
+			}
+		}
+	},
 	Run: func(cmd *cobra.Command, args []string) {
 
 		if showVersion {
