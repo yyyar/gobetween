1d773f83db53dca89b3e2b289e0ba8dc4f51372c
Ants Aasma <ants.aasma@eesti.ee>
2020-01-17 02:17:15 +0200

    fixup: add http healthcheck to manager switch

--- src/manager/manager.go
+++ src/manager/manager.go
@@ -281,6 +281,7 @@ func prepareConfig(name string, server config.Server, defaults config.Connection
 		"ping",
 		"probe",
 		"exec",
+		"http",
 		"none":
 	default:
 		return config.Server{}, errors.New("Not supported healthcheck type " + server.Healthcheck.Kind)
