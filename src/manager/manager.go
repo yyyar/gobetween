80b73cce893370ece034c8b277a80a0bac1d2419
Nick Maliwacki <nmaliwa@microsoft.com>
2020-01-17 22:02:56 -0800

    Add rest api onto update backends

--- src/manager/manager.go
+++ src/manager/manager.go
@@ -217,8 +217,8 @@ func Create(name string, cfg config.Server) error {
 	return nil
 }
 
-// Modify allows you to update the backends of an existing server
-func Modify(name string, backends *[]core.Backend) error {
+// UpdateBackends allows you to update the backends of an existing server
+func UpdateBackends(name string, backends *[]core.Backend) error {
 	servers.RLock()
 	server, ok := servers.m[name]
 	servers.RUnlock()
