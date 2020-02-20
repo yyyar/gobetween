a5cf14b7e334411c359c0951999c88db5c09903f
B Gabbard <Sm0k3scr33n@users.noreply.github.com>
2020-02-20 12:45:21 -0700

    fixing a typo
    fixing a typo for the key_pass_path and key_pass in the Tls Struct

--- src/config/config.go
+++ src/config/config.go
@@ -169,8 +169,8 @@ type Tls struct {
 	AcmeHosts []string `toml:"acme_hosts" json:"acme_hosts"`
 	CertPath  string   `toml:"cert_path" json:"cert_path"`
 	KeyPath   string   `toml:"key_path" json:"key_path"`
-	key_pass_path  *string `toml:"key_path" json:"key_pass_path"`
-	key_pass       *string `toml:"key_path" json:"key_pass"`
+	key_pass_path  string `toml:"key_path" json:"key_pass_path"`
+	key_pass       string `toml:"key_path" json:"key_pass"`
 	tlsCommon
 }
 
