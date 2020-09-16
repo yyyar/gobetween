06dd576d8335f4114bd5ace23b8536947efa21b4
Federico Perez <federico.g.perez@despegar.com>
2020-09-16 15:06:06 -0300

    Added cache-hc to avoid healthchecking 30 times

--- cloudia/hc.sh
+++ cloudia/hc.sh
@@ -6,8 +6,10 @@ curl -v --fail --silent --output /dev/null http://$URL:$PORT
 
 if [ $? -eq 0 ]
 then
+ echo -n 1 > last-hc-result.tmp
  echo -n 1 #success
 else
+ echo -n 1 > last-hc-result.tmp
  echo -n 0 #failure
 fi
 
--- generateTeamsConfig.js
+++ generateTeamsConfig.js
@@ -1,6 +1,6 @@
 
 const teams = [
-  {"port": 9090, "team": "fwsu"},
+  {"port": 9090, "team": "fwsu", "isTrueHealthCheck": true},
   {"port": 9091, "team": "calidad"},
   {"port": 9092, "team": "platform-cancel"},
   {"port": 9093, "team": "platform-transactions"},
@@ -22,14 +22,25 @@ const teams = [
   {"port": 9109, "team": "cambios-air"}
 ];
 
-function createConfig(teams) {
-  const template = '[servers.{team}]\n bind = "0.0.0.0:{port}"\n protocol = "udp"\n balance = "roundrobin"\n backend_idle_timeout="0"\n client_idle_timeout="0"\n\n[servers.{team}.udp]\n max_requests = 1\n max_responses = 0\n\n[servers.{team}.discovery]\n kind = "static"\n static_list = [\n  "as-logstash-00:{port}",\n  "as-logstash-01:{port}"\n ]\n\n[servers.{team}.healthcheck]\n interval = "30s"\n kind = "exec"\n exec_command = "./hc.sh"\n exec_expected_positive_output = "1"\n exec_expected_negative_output = "0"\n timeout = "5s"';
+function getScriptName(isTrueHc){
+  return isTrueHc ? "hc.sh" : "cache-hc.sh";
+}
+
+function createTeamsConfig(teams) {
+  const template = '[servers.{team}]\n bind = "0.0.0.0:{port}"\n protocol = "udp"\n balance = "roundrobin"\n backend_idle_timeout="0"\n client_idle_timeout="0"\n\n[servers.{team}.udp]\n max_requests = 1\n max_responses = 0\n\n[servers.{team}.discovery]\n kind = "static"\n static_list = [\n  "as-logstash-00:{port}",\n  "as-logstash-01:{port}"\n ]\n\n[servers.{team}.healthcheck]\n interval = "30s"\n kind = "exec"\n exec_command = "./{scriptName}"\n exec_expected_positive_output = "1"\n exec_expected_negative_output = "0"\n timeout = "5s"';
   return teams.map(t => {
     let conf;
     conf = template.replace(new RegExp("{team}","g"),t.team);
+    conf = conf.replace(new RegExp("{scriptName}","g"),getScriptName(t.isTrueHealthCheck));
     return conf.replace(new RegExp("{port}","g"),t.port);
   })
   .reduce( (a,b) => a + '\n\n' + b, "" );
+}
+
+function createConfig(teams) {
+  const prefix = '[api]\nenabled = true\nbind = ":9290"\n';
+
+  return prefix + createTeamsConfig(teams);
 }
 
 console.log(createConfig(teams));
